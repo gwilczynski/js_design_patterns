4d7427603da8a56141e3a5b4b5b3faf3a6d9c5b3
gwilczynski <grzegorz.wilczynski@gmail.com>
2012-03-04 17:02:46 +0100

    small refactoring of methods: subscribe and publish

--- observer.js
+++ observer.js
@@ -1,13 +1,6 @@
-/*
-Observer Pattern Implementation
-http://twitter.com/wilq_
-Licensed under the GPL
-*/
-
 var Twitter = (function(){
 
     var userCollection = {};
-    var subscribersId = -1;
     
     return {
         
@@ -17,29 +10,25 @@ var Twitter = (function(){
                 return false;
             }
     
-            var subscribers = userCollection[author];
-            var len = subscribers ? subscribers.length : 0;
-    
-            while (len--) {
-                subscribers[len].user.Read(author, tweet);
+            var followers = userCollection[author];
+            var len = followers ? followers.length : 0;
+
+            for(var i = 0; i < len; i++){
+                followers[i].Read(author, tweet);
             }
     
             return true;
-    
         },
     
-        subscribe: function (user, author) {
+        subscribe: function (user, whoToFollow) {
     
-            if (!userCollection[author]) {
-                userCollection[author] = [];
+            if (!userCollection[whoToFollow]) {
+                userCollection[whoToFollow] = [];
             }
-    
-            var token = (++subscribersId).toString();
-            userCollection[author].push({
-                token: token,
-                user: user
-            });
-            return token;
+            
+            userCollection[whoToFollow].push(user);
+            
+            return true;
         },
     
         unsubscribe: function (token) {
@@ -56,15 +45,19 @@ var Twitter = (function(){
             return false;
         }
     };
-})();
+})(); 
 
 var User = function(name, nick){
     this.name = name;
     this.nick = nick;
 };
 
-User.prototype.Follow = function(follow){
-    Twitter.subscribe(this, follow);
+User.prototype.Follow = function(whoToFollow){
+    Twitter.subscribe(this, whoToFollow);
+};
+
+User.prototype.UnFollow = function(whoToUnFollow){
+	Twitter.unsubscribe(this, whoToUnFollow);
 };
 
 User.prototype.Read = function(author, tweet){
@@ -80,7 +73,6 @@ User.prototype.Write = function(tweet){
 var wilq_ = new User("Grzegorz Wilczynski", "wilq_");
 wilq_.Follow("ODevRel");
 wilq_.Follow("ChromiumDev");
-wilq_.Follow("jank");
 
 var oDevRel = new User("Opera Dev Relations", "ODevRel");
 var chromiumDev = new User("Chrome Dev Relations", "ChromiumDev");
@@ -99,6 +91,7 @@ setTimeout(function(){
     chromiumDev.Write('Chrome DevTools has a slew of new ways to navigate your JavaScript...');
 }, 10000);
 
+
 setTimeout(function(){
     ie.Write('Get all of the latest info on #IE10 in the #MSDN');
 }, 15000);
@@ -106,6 +99,7 @@ setTimeout(function(){
 setTimeout(function(){
     oDevRel.Write('Opera Mobile 12: WebGL, camera access, #html5 parser+more...');
 }, 20000);
+
 
 setTimeout(function(){
     wilq_.Write('RT@ChromiumDev Chrome DevTools has a slew of new ways to navigate your JavaScript...');
